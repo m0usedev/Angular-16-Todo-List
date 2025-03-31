6f4223d4910c3aba6c3d084228d64b47384b7c05
m0usedev <alvaro.sobrino.coding@gmail.com>
2025-03-31 21:22:51 +0200

    some changes app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToDoElementComponent } from './components/to-do-element/to-do-element.component';
+import { ViewAllComponent } from './components/view-all/view-all.component';
+import { ViewAllModule } from './components/view-all/view-all.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ViewAllModule
   ],
   providers: [],
   bootstrap: [AppComponent]
