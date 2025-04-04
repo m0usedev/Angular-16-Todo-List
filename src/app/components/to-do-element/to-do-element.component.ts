9e6198972c5980484bc182dc4302f6a3ccbf9d67
m0usedev <alvaro.sobrino.coding@gmail.com>
2025-04-04 09:29:17 +0200

    add todo-list-service to to-do-element

--- src/app/components/to-do-element/to-do-element.component.ts
+++ src/app/components/to-do-element/to-do-element.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { TodoListService } from 'src/app/services/todo-list.service';
 
 @Component({
   selector: 'app-to-do-element',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./to-do-element.component.css']
 })
 export class ToDoElementComponent {
+  @Input() taskIndex : number | null = null
 
+  constructor (private todoService : TodoListService) {}
+
+  delTask(): void {
+    if (this.taskIndex != null &&
+      Number.isInteger(this.taskIndex) &&
+      this.taskIndex >= 0) {
+        this.todoService.delTask(this.taskIndex);
+      }
+  }
 }
