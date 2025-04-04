9c99c86a7f4b6543ba01b48598b639fc614a05a9
m0usedev <alvaro.sobrino.coding@gmail.com>
2025-04-04 09:28:06 +0200

    add todo-list-service

---
+++ src/app/services/todo-list.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoListService {
+  private tasks : string[] = [];
+  private taksSubject: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(this.tasks);
+  public tasks$: Observable<string[]> = this.taksSubject.asObservable();
+
+  addTask(task: string): void {
+    this.tasks.push(task);
+    this.taksSubject.next([... this.tasks]);
+  }
+
+  delTask(index: number): void {
+    this.tasks.splice(index, 1);
+    this.taksSubject.next([... this.tasks]);
+  }
+}
